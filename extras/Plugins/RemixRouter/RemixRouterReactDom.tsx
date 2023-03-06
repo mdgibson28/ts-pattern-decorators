6ad1aec9be597bf20d276112e1d2ceb614f29691
mdgibson28 <mdgibson28@gmail.com>
2023-03-05 20:58:32 -0500

    chore: remove unused packages

--- extras/Plugins/RemixRouter/RemixRouterReactDom.tsx
+++ extras/Plugins/RemixRouter/RemixRouterReactDom.tsx
@@ -1,36 +1,36 @@
-import { RouterPlugin } from "../../../src/Interfaces/Plugins/Router/RouterPlugin.abstract";
-import { RouterPluginConfig } from "../../../src/Interfaces/Plugins/Router/RouterPluginConfig.interface";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import { getMetadata } from "../../../src/Helpers/Metadata";
-import { ROUTE_CONFIG_TOKEN } from "../../../src/Constants";
+// import { RouterPlugin } from "../../../src/Interfaces/Plugins/Router/RouterPlugin.abstract";
+// import { RouterPluginConfig } from "../../../src/Interfaces/Plugins/Router/RouterPluginConfig.interface";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import "./index.css";
+// import { getMetadata } from "../../../src/Helpers/Metadata";
+// import { ROUTE_CONFIG_TOKEN } from "../../../src/Constants";
 
-/**
- * @class RemixRouterReactDom
- * @description React Router DOM Plugin
- * @extends RouterPlugin
- * @param {RemixRouterReactDomConfig} config
- */
-export class RemixRouterReactDom extends RouterPlugin {
-    constructor(protected config: any) {
-        super(config);
-    }
+// /**
+//  * @class RemixRouterReactDom
+//  * @description React Router DOM Plugin
+//  * @extends RouterPlugin
+//  * @param {RemixRouterReactDomConfig} config
+//  */
+// export class RemixRouterReactDom extends RouterPlugin {
+//     constructor(protected config: any) {
+//         super(config);
+//     }
 
-    public register(): void {
-        const { routes } = this.config;
+//     public register(): void {
+//         const { routes } = this.config;
 
-        const router = createBrowserRouter(
-            (routes || []).map((route) => {
-                return getMetadata(route, ROUTE_CONFIG_TOKEN);
-            })
-        );
+//         const router = createBrowserRouter(
+//             (routes || []).map((route) => {
+//                 return getMetadata(route, ROUTE_CONFIG_TOKEN);
+//             })
+//         );
 
-        ReactDOM.createRoot(this.config.attachTo).render(
-            <React.StrictMode>
-                <RouterProvider router={router} />
-            </React.StrictMode>
-        );
-    }
-}
+//         ReactDOM.createRoot(this.config.attachTo).render(
+//             <React.StrictMode>
+//                 <RouterProvider router={router} />
+//             </React.StrictMode>
+//         );
+//     }
+// }
